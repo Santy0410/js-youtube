dee2c71e4eef62fadaae319e893bc1e4c3fccc35
SANTOSH PANDA T <103490878+Santy0410@users.noreply.github.com>
2023-12-11 09:09:22 +0000

    variables and datatypes done

---
+++ program/01_variables.js
@@ -0,0 +1,11 @@
+const accountid = 144553
+let accountEmail = "hitesh@goole.com"
+var accountPassword = "12345"
+let accountstate
+/* 
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+accountcit = "jaipur"/*not to use this type */
+console.log(accountid)
+console.table([accountid, accountEmail,accountPassword,accountcit,accountstate])
---
+++ program/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello") // we r using node js not browser
+console.log(4-5)
+
+let name = "santy"
+let age = 21
+let islogin = false
+// primitive datatypes
+// number => 2 to power 53
+//bigint 
+// string = ""
+// boolean = true/false
+// null => standalone value
+// undefined => variable declared but value not assigned 
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof undefined);// undfined
+console.log(typeof null);// 
